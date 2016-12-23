cac3eb7cca04cb92b1689fd8f27d296d68d6a2f5
Harold Angenent <harold@trendwerk.nl>
2016-12-23 14:31:35 +0100

    ✨ Install `php7.0-curl` module

--- provision/php/install-php7.sh
+++ provision/php/install-php7.sh
@@ -8,7 +8,7 @@ sudo apt-get install php7.0 -y &> /dev/null
 echo 'Updating apt-get repositories...'
 sudo apt-get update &> /dev/null
 echo 'Installing php7.0 modules...'
-sudo apt-get install php7.0-mysql libapache2-mod-php7.0 php7.0-xml php7.0-mbstring -y &> /dev/null
+sudo apt-get install php7.0-mysql libapache2-mod-php7.0 php7.0-xml php7.0-mbstring php7.0-curl -y &> /dev/null
 echo 'Disabling php5...'
 sudo a2dismod php5 &> /dev/null
 echo 'Enabling php7.0...'
