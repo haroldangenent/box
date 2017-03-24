1512cef29b32f4c441e4ff4352b01f0ab0d75e57
Harold Angenent <harold@trendwerk.nl>
2017-03-24 10:51:04 +0100

    :arrow_up: PHP 7.1

--- provision/php/install-php7.sh
+++ provision/php/install-php7.sh
@@ -1,18 +1,18 @@
-echo 'Installing php7.0...'
+echo 'Installing php7.1...'
 echo 'Updating apt-get repositories...'
 sudo apt-get update &> /dev/null
-echo 'Adding php7.0...'
+echo 'Adding php7.1...'
 sudo add-apt-repository ppa:ondrej/php -y &> /dev/null
-echo 'Installing php7.0...'
-sudo apt-get install php7.0 -y &> /dev/null
+echo 'Installing php7.1...'
+sudo apt-get install php7.1 -y &> /dev/null
 echo 'Updating apt-get repositories...'
 sudo apt-get update &> /dev/null
-echo 'Installing php7.0 modules...'
-sudo apt-get install php7.0-mysql libapache2-mod-php7.0 php7.0-xml php7.0-mbstring php7.0-curl php7.0-xdebug -y &> /dev/null
+echo 'Installing php7.1 modules...'
+sudo apt-get install php7.1-mysql libapache2-mod-php7.1 php7.1-xml php7.1-mbstring php7.1-curl php7.1-xdebug -y &> /dev/null
 echo 'Disabling php5...'
 sudo a2dismod php5 &> /dev/null
-echo 'Enabling php7.0...'
-sudo a2enmod php7.0 &> /dev/null
+echo 'Enabling php7.1...'
+sudo a2enmod php7.1 &> /dev/null
 echo 'Restarting webserver...'
 sudo service apache2 restart &> /dev/null
-echo 'Finished installing php7.0.'
+echo 'Finished installing php7.1.'
