8773fdb833d741ba2b7c2f27c885d4aff4e8cc75
Harold Angenent <harold@trendwerk.nl>
2017-03-28 17:47:11 +0200

    :bug: Add new line in between hostnames and aliases

--- provision/vhosts/hosts-get.sh
+++ provision/vhosts/hosts-get.sh
@@ -1,7 +1,7 @@
 getHosts() {
 	hostnames=$(vagrant ssh -c "/usr/sbin/apache2ctl -S 2>&1 | awk '/namevhost/' | awk '{print \$4;}'")
 	aliases=$(vagrant ssh -c "/usr/sbin/apache2ctl -S 2>&1 | awk '/alias/' | awk '{print \$2;}'")
-	echo "$hostnames$aliases"
+	printf "$hostnames\n$aliases"
 }
 
 getIp() {
